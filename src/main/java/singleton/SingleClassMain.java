0872db155e1b527bd44912a2647099e7786d8c91
imtdeepak <imt.deepak@gmail.com>
2016-05-21 23:53:04 -0700

    adding pattern code

---
+++ src/main/java/singleton/SingleClassMain.java
@@ -0,0 +1,94 @@
+package singleton;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ * Created by deep on 5/20/16.
+ */
+public class SingleClassMain {
+
+    static void useSingleton(){
+//        Singleton singleton = Singleton.INSTANCE;
+        Singleton singleton= Singleton.getInstance();
+        printObject("singleton",singleton);
+    }
+
+    public static void main(String args[]) throws Exception
+
+    {
+
+        System.out.println("Using MultiThread");
+
+        ExecutorService execService= Executors.newFixedThreadPool(2);
+        execService.submit(SingleClassMain::useSingleton);
+        execService.submit(SingleClassMain::useSingleton);
+
+        execService.shutdown();
+
+
+        Singleton s1 = Singleton.getInstance();
+
+        Singleton s2 = Singleton.getInstance();
+
+        //Same instances
+        printObject("s1",s1);
+        printObject("s2",s2);
+
+
+
+        //Reflection
+//        System.out.println("Using reflection");
+//
+//        Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Singleton s3=constructor.newInstance();
+//        printObject("s3",s3);
+
+
+        //Using Serialization
+
+        System.out.println("Using Serialization/Deserialization");
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("/tmp/s1.ser"));
+        objectOutputStream.writeObject(s1);
+        objectOutputStream.close();
+
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("/tmp/s1.ser"));
+
+        Singleton s4= (Singleton) objectInputStream.readObject();
+
+        printObject("s4",s4);
+
+
+
+
+
+
+    }
+
+
+
+    public static void printObject(String name, Object obj) {
+        if (obj != null) {
+            System.out.println("Hashcode of object "+name +"= " + obj.hashCode());
+        }
+    }
+}
+
+//Enum can also serve all the purpose of Singleton behavior and is
+// Reflection, Serialization/Deserialization   and Multi threading safe
+//
+// enum Singleton{
+//
+//    INSTANCE:
+//    public String getConfiguration{
+//        System.out.println("ALpha Beeta");
+//    }
+//
+//}
---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,67 @@
+package singleton;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+/**
+ * Created by deep on 5/20/16.
+ */
+public class Singleton implements Serializable,Cloneable{
+
+//    private static Singleton oneInstance=null;
+
+    private static volatile  Singleton oneInstance=null;
+
+    private Singleton() {
+        if (oneInstance != null) {
+            throw new RuntimeException(" Can not create Single Instance object once again");
+        }
+        //proceed
+        System.out.println("Creating Singleton instance");
+    }
+
+
+    //Synchronized whole getInstance method -- Safe but not efficient
+//    public static synchronized Singleton getInstance() {
+//
+//        if(oneInstance==null){
+//            oneInstance=new Singleton();
+//
+//        }
+//        return oneInstance;
+//    }
+
+
+    //Double check --Works mostly but will break
+//    public static Singleton getInstance() {
+//
+//        if(oneInstance==null){//Check 1
+//            synchronized(Singleton.class){
+//                if(oneInstance==null) {//Check 2
+//                    oneInstance = new Singleton();
+//                }
+//            }
+//
+//
+//        }
+//        return oneInstance;
+//    }
+
+    static class Holder{
+        static final Singleton INSTANCE=new Singleton();
+    }
+    public static Singleton getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    private Object readResolve() throws ObjectStreamException{
+        System.out.println("Deserializing resolve");
+        return oneInstance;
+
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
