b80ba00d1937d58b779215ea0607d609764dec27
Deepak Kumar <pair+deepak.kumar6@ge.com>
2016-07-14 22:20:01 -0700

    Added Fibonacii

--- src/main/java/java8/stream/Fibonacii.java
+++ src/main/java/java8/stream/Fibonacii.java
@@ -7,14 +7,36 @@ import java.util.Map;
  * Created by byjumanikkan on 2/6/16.
  */
 public class Fibonacii {
-    static Map<Integer, Integer> map = new HashMap<>();
-    static{
-        map.put(0,0);
-        map.put(1,1);
-    }
+    private static Map<Integer,Integer> fibSum=new HashMap<>();
+
     public static int fibonacci(int x){
-        return 0;
+        if(x==0){
+            fibSum.put(0,0);
+            return 0;
+        }else if(x==1){
+            fibSum.put(1,1);
+            return 1;
+        }else{
+            Integer sum1=fibSum.get(x-1);
+            Integer sum2 = fibSum.get(x-2);
+            if(sum1==null){
+                sum1=fibonacci(x-1);
+                fibSum.put(x-1,sum1);
+            }
+            if (sum2==null){
+                sum2=fibonacci(x-2);
+                fibSum.put(x-2,sum2);
+            }
+
+            return sum1+sum2;
+        }
+
+
     }
     public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println("Fibonacci of "+i+" = "+ fibonacci(i));
+        }
+
     }
 }
